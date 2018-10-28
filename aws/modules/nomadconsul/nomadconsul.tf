63ef23495975ca8315e6aa164ca9a47a71056d4e
rberlind <roger@hashicorp.com>
2018-10-28 14:07:38 -0400

    add rules for port 8080

--- aws/modules/nomadconsul/nomadconsul.tf
+++ aws/modules/nomadconsul/nomadconsul.tf
@@ -205,6 +205,24 @@ resource "aws_security_group_rule" "nomad_dynamic_ports_egress" {
     source_security_group_id = "${aws_security_group.primary.id}"
 }
 
+resource "aws_security_group_rule" "catalogue_ingress" {
+    security_group_id = "${aws_security_group.primary.id}"
+    type = "ingress"
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    source_security_group_id = "${aws_security_group.primary.id}"
+}
+
+resource "aws_security_group_rule" "catalogue_egress" {
+    security_group_id = "${aws_security_group.primary.id}"
+    type = "egress"
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    source_security_group_id = "${aws_security_group.primary.id}"
+}
+
 resource "aws_security_group_rule" "https_egress" {
     security_group_id = "${aws_security_group.primary.id}"
     type = "egress"
